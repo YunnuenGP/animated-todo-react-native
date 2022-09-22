0d058a6c54e4700608dd38c9882718ad58a6f911
Walter NEA <walter@nationaleczema.org>
2022-09-22 15:56:04 -0500

    Adding checkbox animation, default theme and dark-theme support

---
+++ src/screens/main.tsx
@@ -0,0 +1,25 @@
+import { Text, Box, Center, VStack, themeTools, useTheme, useColorMode, useColorModeValue } from 'native-base';
+import ThemeToggle from '../components/theme-toggle';
+import TaskItem from '../components/task-item';
+import { useCallback, useState } from 'react';
+
+export default function MainScreen(){
+    const [checked, setChecked] = useState(false);
+    const handlePressCheckbox = useCallback(() => {
+        setChecked(prev => !prev);
+    }, []);
+
+    return (
+        <Center _dark={{bg: 'blueGray.900'}} _light={{bg: 'blueGray.50'}} px={4} flex={1}>
+            <VStack space={5} alignItems="center">
+                <Box w="100px" h="100px">
+                    <TaskItem isDone={checked} onToggleCheckbox={handlePressCheckbox}/>
+                </Box>
+                <Box p={10} bg={useColorModeValue('red.500', 'yellow.500')}>
+                    <Text>Hello World!!</Text>
+                </Box>
+                <ThemeToggle />
+            </VStack>
+        </Center>
+    )
+}
